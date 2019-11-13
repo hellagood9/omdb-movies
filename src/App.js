a75a8dd2efc1e4fede6e7f66ce06da3bfebb7229
Maximiliano Sussini <maxsus60@gmail.com>
2019-11-13 19:49:18 +0100

    Bulma installed, Title element created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bulma/css/bulma.css";
+
+import { Title } from "./elements/Title"
 
 function App() {
   return (
     <div className="App">
-     <h1>Search Movies</h1>
+      <Title>Search Movies</Title>
     </div>
   );
 }
